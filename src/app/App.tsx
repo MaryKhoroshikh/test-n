171368b3b5984b2bc79d7c9d0890af205be28760
Maria Khoroshikh <goodmary7@yandex.ru>
2025-11-25 17:25:10 +0300

    section ui

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,7 @@
 import './App.css'
 import ButtonCTA from '../ui/buttonCTA/buttonCTA'
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+import Section from '../ui/section/section';
 
 function App() {
 
@@ -13,9 +15,26 @@ function App() {
 
   return (
     <>
-      <ButtonCTA onClick={hundleGuideToForm}/>
+      <Section 
+        title='Подготовьте вашу команду к эпохе ИИ'
+        type='main'
+        text='Эпоха искусственного интеллекта уже наступила — убедитесь, что ваша команда готова к новым вызовам.
+        Узнайте, как развивать ключевые навыки и эффективно внедрять ИИ-технологии в рабочие процессы'
+      >
+        <ButtonCTA onClick={hundleGuideToForm}/>
+        <DotLottieReact
+          src="/src/assets/test.lottie"
+          loop={true}
+          autoplay={true}
+          style={{
+            maxWidth: '200px',
+            width: '100%',
+            height: 'auto',
+          }}
+        /> 
+      </Section>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/ui/section/section.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import type { ReactNode } from 'react';
+import styles from './section.module.css';
+
+interface SectionProps {
+  title: string;
+  type?: 'main' | 'ext'; // по умолчанию будем использовать 'sub'
+  text?: string;
+  children?: ReactNode;
+}
+
+const Section: React.FC<SectionProps> = ({ title, type = 'ext', text, children }) => {
+  const HeadingTag = type === 'main' ? 'h1' : 'h2';
+
+  return (
+    <section className={styles.section}>
+      <HeadingTag className={styles.title}>{title}</HeadingTag>
+      {text && <p className={styles.text}>{text}</p>}
+      {children && <div className={styles.content}>{children}</div>}
+    </section>
+  );
+};
+
+export default Section;
